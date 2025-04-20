aa56d0829148281d4359a411f6a9dd8cd7a3a11c
KimBoYoung <qhdud20@naver.com>
2025-04-20 17:03:38 +0900

    feat: 상품 타입 추가 및 디자인 수정

---
+++ components/product/ProductList.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useRef, useState } from "react";
+import ProductCard from "./ProductCard";
+import styles from "./ProductList.module.scss";
+import { Product, ViewType } from "@/types/product";
+import { ClipLoader } from "react-spinners";
+
+interface ProductListProps {
+  initialProducts: Product[];
+  initialViewType: ViewType;
+  query: string;
+  sort: string;
+}
+
+export default function ProductList({ initialProducts, initialViewType, query, sort }: ProductListProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+  const [viewType, setViewType] = useState<ViewType>(initialViewType);
+  const [hasMore, setHasMore] = useState(true);
+  const skipRef = useRef(initialProducts.length);
+
+  return (
+    <section className={styles.container}>
+      {products.length === 0 ? (
+        <div className={styles.message}>일치하는 결과가 없습니다.</div>
+      ) : (
+        <ul className={viewType === "grid" ? styles.grid : styles.list}>
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} viewType={viewType} />
+          ))}
+        </ul>
+      )}
+
+      {isLoading && (
+        <div className={styles.loadingWrapper}>
+          <ClipLoader color="#3b82f6" loading={isLoading} size={60} />
+        </div>
+      )}
+      {!hasMore && <div className={styles.message}>더 이상 불러올 수 없습니다.</div>}
+    </section>
+  );
+}
